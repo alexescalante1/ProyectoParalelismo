abf03fa6301d6deb0ce9b3c8dd2a39de1f397649
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2020-08-14 15:07:40 -0500

    Update index.php
    hola chulls

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-Hello esta es una Prueva
-que sad papus
-haber pendejos ahora me ven
-me pueden ver?
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola mundo</title>
+    <h1>que tal people</h1>
+</head>
+<body>
+    
+</body>
+</html>
+
