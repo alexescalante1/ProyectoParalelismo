713727b80fbf7910aa4f938a815389aa961e337e
alexescalante1 <39635523+alexescalante1@users.noreply.github.com>
2020-08-14 23:05:31 -0500

    Creando Estructura Cliente bajo el modelo vista controlador x2

--- Cliente/vistas/plantilla.php
+++ Cliente/vistas/plantilla.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Virtual</title>
+    <title>Mi Tienda Virtual</title>
 </head>
 <body>
     <h1>Hola mundo</h1>
