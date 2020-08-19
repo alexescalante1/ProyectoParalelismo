02ce98b273d2f771a4ab9233e07b8ffdbe21253f
alexescalante1 <39635523+alexescalante1@users.noreply.github.com>
2020-08-19 09:38:10 -0500

    Agregando bootstrap y jquery and navar

--- Cliente/vistas/plantilla.php
+++ Cliente/vistas/plantilla.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Tienda Virtual</title>
+    <meta charset = "UTF-8">
+
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    
+    <meta name = "title" content = "Tienda Virtual">
+
+    <meta name = "description" content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ea earum. Distinctio">
+    
+    <meta name = "keyword" content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ea earum. Distinctio">
+
+    <title>Tienda Virtual</title>
+
+
+    <link rel = "stylesheet" href = "vistas/css/plugins/bootstrap.css">
+
+    <script src = "vistas/js/plugins/jquery.min.js"></script>
+
+    <script src = "vistas/js/plugins/bootstrap.js"></script>
+
+
 </head>
 <body>
-    <h1>Hola mundo</h1>
+
+<?php
+
+/*incluimos a cabezote*/
+
+include "modulos/cabezote.php";
+
+?>
+
 </body>
 </html>
