a0152ef3b91c76d258a7a8f911a2087fea299883
alexescalante1 <39635523+alexescalante1@users.noreply.github.com>
2020-09-24 14:16:33 -0500

    AgregandoCentrodeMensajes-SoporteCliente

---
+++ CentroDeMensajes/vistas/plantilla.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<meta name="title" content="Tienda Virtual">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam accusantium enim esse eos officiis sit officia">
+
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
+
+	<title>Tienda Virtual</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?php
+
+	include "modulos/cabezote.php";
+
+}
+
+?>
+
+</body>
+</html>
