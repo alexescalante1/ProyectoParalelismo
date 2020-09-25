59dc5b3adb168f69825793d11a15ee25cd9759d7
alexescalante1 <39635523+alexescalante1@users.noreply.github.com>
2020-09-25 12:59:51 -0500

    RedireccionCarrusel

--- Cliente/vistas/modulos/carrusel.php
+++ Cliente/vistas/modulos/carrusel.php
@@ -15,7 +15,11 @@
 					<div class="carousel-inner" role="listbox">
 					
 						<div class="item active">
+							<a href="https://www.youtube.com/watch?v=YQRHrco73g4">
 							<img src="<?php echo $url; ?>vistas/img/portada/c1.png" alt="...">
+							</a>
+							
+							
 							<div class="carousel-caption">
 						
 							</div>
