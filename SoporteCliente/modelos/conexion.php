0e8eceeb0807906fd1755b6a4b47627d61d76b11
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2020-10-04 17:55:42 -0500

    haciendo ressponsive la pagina

--- SoporteCliente/modelos/conexion.php
+++ SoporteCliente/modelos/conexion.php
@@ -3,9 +3,22 @@
 class Conexion{
 
 	public function conectar(){
+		try{
+			$linkBD = new PDO('mysql:host=localhost;dbname=alistore',
+					'root','',
+					array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+					PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") ///Trae y exporta los caracteres latinos
+			  );
+			echo "conexion OK";
+		}catch(Exception $e){
+			die('error: '. $e->getMessage());
+		}
+		finally{
+			$linkBD = null;
+		}
 
-
-
+		return $linkBD;
+			
 	}
 
 
