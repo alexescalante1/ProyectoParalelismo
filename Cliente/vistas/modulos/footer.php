bafc751cec5cb6e44767c9fa6cc399828b504a24
ch4r4j4 <68834265+ch4r4j4@users.noreply.github.com>
2020-10-04 22:06:07 -0500

    Update footer.php
    Ya estoy subiendo esto habers que pasa

--- Cliente/vistas/modulos/footer.php
+++ Cliente/vistas/modulos/footer.php
@@ -1,1 +1,87 @@
-<h1 class = "TEST_ESTY"> okas papi </h1>
+<footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
+
+                <div class="colum1">
+                    <h1>Mas informacion de Mi Store</h1>
+
+                    <p>Mi store es una tienda online dedicada a la venta de diferentes articulos. como tecnologia ropa y muhas cosas mas en diferentes variedades </p>
+
+                </div>
+
+                <div class="colum2">
+
+                    <h1>Redes Sociales</h1>
+
+                    <div class="row">
+                        <img src="icon/facebook.png">
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row">
+                        <img src="icon/twitter.png">
+                        <label>Siguenos en Twitter</label>
+                    </div>
+                    <div class="row">
+                        <img src="icon/instagram.png">
+                        <label>Siguenos en Instagram</label>
+                    </div>
+                   <!---
+                    <div class="row">
+                        <img src="icon/google-plus.png">
+                        <label>Siguenos en Google Plus</label>
+                    </div>
+                    <div class="row">
+                        <img src="icon/pinterest.png">
+                        <label>Siguenos en Pinteres</label>
+                    </div>
+                    -->
+
+                </div>
+
+                <div class="colum3">
+
+                    <h1>Informacion Contactos</h1>
+
+                    <div class="row2">
+                        <img src="icon/house.png">
+                        <label>Tiahuanaco,
+                        numero 501 
+                        Puno-Peru
+                        Barrio Huajzapata</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="icon/smartphone.png">
+                        <label>+51 934 621 212
+                        +51 983 762 574
+                        +51 910 106 752
+                        +51 912 805 678</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="icon/contact.png">
+                         <label>mistore@gmail.com</label>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2020 Todos los Derechos Reservados | <a href="">MiStore</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
