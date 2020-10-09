69a0b427828a6d394fbeef2d2f450260eba410d0
ch4r4j4 <68834265+ch4r4j4@users.noreply.github.com>
2020-10-09 10:15:38 -0500

    CreoelultimoCommit

--- ValoracionesProducto/index.php
+++ ValoracionesProducto/index.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="vistas/css/misEstilos.css">
     <link href="https://fonts.googleapis.com/css?family=Mukta+Vaani" rel="stylesheet">
     
+    <link rel="stylesheet" href="<?php echo $url?>vistas/css/estilos.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
     <script type="text/javascript">
         function ajax(){
             var req= new XMLHttpRequest();
@@ -51,7 +54,81 @@
             }
         ?>
     </div>    
+    <footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
 
+                <div class="colum1">
+                    <h1>Mas informacion de Mi Store</h1>
+
+                    <p>Mi store es una tienda online dedicada a la venta de diferentes articulos. como tecnologia ropa y muhas cosas mas en diferentes variedades </p>
+
+                </div>
+
+                <div class="colum2">
+
+                    <h1>Redes Sociales</h1>
+
+                    <div class="row">
+                        <img src="vistas/icon/facebook.png">
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row">
+                        <img src="vistas/icon/twitter.png">
+                        <label>Siguenos en Twitter</label>
+                    </div>
+                    <div class="row">
+                        <img src="vistas/icon/instagram.png">
+                        <label>Siguenos en Instagram</label>
+                    </div>
+                   
+
+                </div>
+
+                <div class="colum3">
+
+                    <h1>Informacion Contactos</h1>
+
+                    <div class="row2">
+                        <img src="vistas/icon/house.png">
+                        <label>Tiahuanaco,
+                        numero 501 
+                        Puno-Peru
+                        Barrio Huajzapata</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="vistas/icon/smartphone.png">
+                        <label>+51 934 621 212</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="vistas/icon/contact.png">
+                         <label>mistore@gmail.com</label>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2020 Todos los Derechos Reservados | <a href="">MiStore</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
 </body>
 </html>
 
